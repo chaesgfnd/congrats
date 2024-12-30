2f4b118f4198a4bfd0be321b35cce45026a8ddb6
chaesgfnd <m79160164445@gmail.com>
2024-12-30 19:57:21 +0000

    fix: working

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # PROJECT_NAME_PLACEHOLDER
 
-![Lines Of Code](https://img.shields.io/badge/LoC-87-lightblue)
+![Lines Of Code](https://img.shields.io/badge/LoC-77-lightblue)
 
 <!-- markdownlint-disable -->
 <details>
--- src/main.py
+++ src/main.py
@@ -1,15 +1,21 @@
-from ValeraLib import chk # noqa: F401
-from ValeraLib.prelude import *
-from typing import Any, Self, Union, Optional, List, Tuple, Callable, TypeVar, Generic # noqa: F401
+from typing import Any, Self, Union, Optional, List, Tuple, Callable, TypeVar, Generic  # noqa: F401
+
 try:
 	from icecream import ic  # noqa: F401
 except ImportError:  # Graceful fallback if IceCream isn't installed.
 	ic = lambda *a: None if not a else (a[0] if len(a) == 1 else a)  # noqa
 
 
+def generate():
+	s = "happy new year, unless you were naughty. Naughty naughty"
+	return s
+
+
 def main():
-	print("Hello World!")
+	print("INFO: Starting...")
+	msg = generate()
+	ic(msg)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
 	main()
